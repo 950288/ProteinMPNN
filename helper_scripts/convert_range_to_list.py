024fe0e5daea27d4318db0f9cf371b255fd44045
950288 <950288s@gmail.com>
2023-06-26 20:12:47 +0800

    update convert_range_to_list.py

--- helper_scripts/convert_range_to_list.py
+++ helper_scripts/convert_range_to_list.py
@@ -1,6 +1,8 @@
 import argparse
 
+
 def main(args):
+
     pos_list = []
     ranges = args.design_only_positions.split()
     for r in ranges:
@@ -15,6 +17,8 @@ def main(args):
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argparser.add_argument("--input_path", type=str, help="Path to the parsed PDBs")
+    argparser.add_argument("--output_path", type=str, help="Path to the output dictionary")
 
     args = argparser.parse_args()
     main(args)
